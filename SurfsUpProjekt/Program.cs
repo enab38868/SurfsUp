f7916112f17f897395ed9202835c5adc0b2ace29
Sigurd W <sigurd_w@hotmail.com>
2022-08-23 13:16:55 +0200

    Ændret home-page til boards

--- SurfsUpProjekt/Program.cs
+++ SurfsUpProjekt/Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Boards}/{action=Index}/{id?}");
 
 app.Run();
