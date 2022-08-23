9f1c2cc31785b835cf61010ca71ec8f1fca2a61b
enab38868 <enab38868@edu.ucl.dk>
2022-08-23 09:47:34 +0200

    init

---
+++ SurfsUpProjekt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SurfsUpProjekt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
