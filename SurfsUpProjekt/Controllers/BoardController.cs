960e92e01daf22276614e9ca0ccde3844bf58bb2
Sigurd W <sigurd_w@hotmail.com>
2022-08-23 12:24:40 +0200

    MVC

---
+++ SurfsUpProjekt/Controllers/BoardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SurfsUpProjekt.Controllers
+{
+    public class BoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SurfsUpProjekt/Models/Boards.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SurfsUpProjekt.Models;
+
+public class Boards
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Length { get; set; }
+    public double Width { get; set; }
+    public double Thickness { get; set; }
+    public double Volume { get; set; }
+    public string Type { get; set; }
+    public double Price { get; set; }
+    public string? Equipment { get; set; }
+
+}
