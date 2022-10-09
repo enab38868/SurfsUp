0d9019e25f391ad789526e6b6fb2a3d1ced74c11
Tobias <Tobias@DESKTOP-NMKE5TG>
2022-10-09 17:38:26 +0200

    SAD

--- SurfsUpAPI/Controllers/BoardsController.cs
+++ SurfsUpAPI/Controllers/BoardsController.cs
@@ -13,34 +13,33 @@ namespace SurfsUpAPI.Controllers
     [ApiController]
     public class BoardsController : ControllerBase
     {
-        private readonly SurfsUpProjektContext _context;
+        //private readonly SurfsUpProjektContext _context;
 
 
-        public BoardsController(SurfsUpProjektContext context)
+        public BoardsController()
         {
-            _context = context;
-
+           
         }
 
-        [HttpGet, Route("/UserIndex")]
-        public async Task<ActionResult> GetAllBoards()
+        [HttpGet("/UserIndex")]
+        public async Task<IEnumerable<Board>> GetAllBoards()
         {
-            List<Board> boardslist = new List<Board>();
-            string cncstring = ("Server = 10.56.8.36; Database = PEDB10; User Id = PE - 10; Password = OPENDB_10; Trusted_Connection = False; MultipleActiveResultSets = true");
+            List<Board> boardslist = new List<Board>();//Services(mappe). fks Boardserverice 
+            string cncstring = ("Server = 10.56.8.36; Database = PEDB10; User Id = PE - 10; Password = OPENDB_10; Trusted_Connection = False; MultipleActiveResultSets = true"); // REPO mappe //husk at registere i Program.cs
             using (SqlConnection sqlcon = new SqlConnection(cncstring))
             {
-                boardslist = _context.Board.OrderBy(a => a.Name).ToList();
+                //boardslist = _context.Board.OrderBy(a => a.Name).ToList(); // den ´forventer Ienumerable som retur nu
                
-                return Ok(boardslist);
+                return boardslist;
 
             }
             //return Ok(await _context.Board.ToArrayAsync());
         }
-        [HttpGet, Route("/Rents/Details/{id}")]
-        public async Task<ActionResult> GetBoard(int id)
-        {
-            var board = _context.Board.Find(id);
-            return Ok(board);
-        }
+        //[HttpGet, Route("/Rents/Details/{id}")]
+        //public async Task<ActionResult> GetBoard(int id)
+        //{
+        //    //var board = _context.Board.Find(id);
+        //    return Ok(board);
+        //}
     }
 }
--- SurfsUpAPI/Program.cs
+++ SurfsUpAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using SurfsUpProjekt.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
