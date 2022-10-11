f86f6ad196e99ee750a9bb2ebd01aabb58ac33b3
Emil <emil95@hotmail.dk>
2022-10-11 09:28:40 +0200

    model class

---
+++ SurfsUpProjekt/Models/NonUserRent.cs
@@ -0,0 +1,9 @@
+﻿namespace SurfsUpProjekt.Models
+{
+    public class NonUserRent
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
