7865142c36e7e2cf1d64641387117648d34adbe8
Tobiasdanielsen123115 <91379009+Tobiasdanielsen123115@users.noreply.github.com>
2022-11-23 12:34:30 +0100

    asjdj

--- SurfsUpProjekt/Controllers/BoardsController.cs
+++ SurfsUpProjekt/Controllers/BoardsController.cs
@@ -219,7 +219,7 @@ namespace SurfsUpProjekt.Controllers
                            + "was modified by another user after you got the original value. The "
                            + "edit operation was canceled and the current values in the database "
                            + "have been displayed. If you still want to edit this record, click "
-                           + "the Save button again. Otherwise click the Back to List hyperlink.");
+                           + "back and then edit again.. Better safe than sorry ;-) .");
                         boardToUpdate.RowVersion = (byte[])databaseValues.RowVersion;
                         ModelState.Remove("RowVersion");
                     }
